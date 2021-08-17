7b8f3ec585c4209e22614d3f8dfdf7feeb18be14
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-17 14:02:17 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
-    pass
+    import math
+    
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    
+    a = (x1,y1)
+    b = (x2,y2)
+    
+    pendiente = math.dist(b-a)
+    
+    print("Pendiente: ", pendiente)
 
 
 if __name__ == '__main__':
