d339bb4a61ee94380389f171e0c230dfe5866bbf
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-17 14:28:04 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -8,7 +8,7 @@ def main():
     x2 = float(input("Dame x2: "))
     y2 = float(input("Dame y2: "))
     
-   pendiente = (y2-y1)/(x2-x1)
+    pendiente = (y2-y1)/(x2-x1)
 
     print("Pendiente:", pendiente)
 
