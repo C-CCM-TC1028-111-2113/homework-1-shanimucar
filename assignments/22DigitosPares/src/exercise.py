61e2a5dfe86d5c497ec2f316e29fb6f93e5a5d87
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-18 17:56:57 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,7 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    number = int(input("Dame un número: "))
+    lista = list(str(number))
+    digitos = [int(x) for x in lista]
+    
+    par = 0
+    
+    for num in digitos:
+        if num % 2 == 0:
+            par += 1
+        else:
+            par += 0
 
+    print("El número de dígitos pares es ", par)
 
 if __name__ == '__main__':
     main()
